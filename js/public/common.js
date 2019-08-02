ed1c556d8ad1d381e3c6ee414a363fae636e76c6
parasol <879623546@qq.com>
2019-08-02 22:51:27 +0800

    reset

--- js/public/common.js
+++ js/public/common.js
@@ -1,34 +1,34 @@
 //h5适配
-document.write('<script src="./../js/public/flexible.js" type="text/javascript" charset="utf-8"></script>');
+document.write('<script src="../js/public/flexible.js" type="text/javascript" charset="utf-8"></script>');
 //公共基础样式
-document.write('<link rel="stylesheet" href="./../css/base.css?v=20181022">');
-document.write('<link rel="stylesheet" href="./../css/blackDialog.css?v=20181022">');
+document.write('<link rel="stylesheet" href="../css/base.css?v=20181022">');
+document.write('<link rel="stylesheet" href="../css/blackDialog.css?v=20181022">');
 //标题logo
-document.write('<link rel="shortcut icon" href="./../img/favicon.png?v=20181022" type="image/x-icon" />');
+document.write('<link rel="shortcut icon" href="../img/favicon.png?v=20181022" type="image/x-icon" />');
 //JQuery
-document.write('<script src="./../js/jquery-2.1.3.min.js" type="text/javascript" charset="utf-8"></script>');
+document.write('<script src="../js/jquery-2.1.3.min.js" type="text/javascript" charset="utf-8"></script>');
 //AngularJS
-document.write('<script src="./../js/angular.min.js" type="text/javascript" charset="utf-8"></script>');
+document.write('<script src="../js/angular.min.js" type="text/javascript" charset="utf-8"></script>');
 //web3.js
-document.write('<script language="javascript" type="text/javascript" src="./../js/web3/web3.min.js"></script>');
+document.write('<script language="javascript" type="text/javascript" src="../js/web3/web3.min.js"></script>');
 //获取智能合约
-document.write('<script src="./../js/public/getWeb3.js?v=20181022" type="text/javascript" charset="utf-8"></script>');
+document.write('<script src="../js/public/getWeb3.js?v=20181022" type="text/javascript" charset="utf-8"></script>');
 //计算中部内容器高度
-document.write('<script src="./../js/public/resizeContent.js?v=20181022" type="text/javascript" charset="utf-8"></script>');
+document.write('<script src="../js/public/resizeContent.js?v=20181022" type="text/javascript" charset="utf-8"></script>');
 //消除移动设备点击延时的插件
-document.write('<script src="./../js/fastclick.js" type="text/javascript" charset="utf-8"></script>');//消除移动设备点击延时的插件
+document.write('<script src="../js/fastclick.js" type="text/javascript" charset="utf-8"></script>');//消除移动设备点击延时的插件
 //弹窗插件
-document.write('<script src="./../js/layer/layer.js" type="text/javascript" charset="utf-8"></script>');
+document.write('<script src="../js/layer/layer.js" type="text/javascript" charset="utf-8"></script>');
 //dialog插件(自制)&屏蔽层&提示框
-document.write('<script src="./../js/public/loadingDialog.js?v=20181022" type="text/javascript" charset="utf-8"></script>');
-document.write('<script src="./../js/public/blackDialog.js?v=20181022" type="text/javascript" charset="utf-8"></script>');
+document.write('<script src="../js/public/loadingDialog.js?v=20181022" type="text/javascript" charset="utf-8"></script>');
+document.write('<script src="../js/public/blackDialog.js?v=20181022" type="text/javascript" charset="utf-8"></script>');
 //cookie
-document.write('<script src="./../js/jquery.cookie.js" type="text/javascript" charset="utf-8"></script>');
+document.write('<script src="../js/jquery.cookie.js" type="text/javascript" charset="utf-8"></script>');
 //表单校验
-document.write('<script src="./../js/public/valid.js" type="text/javascript" charset="utf-8"></script>');
+document.write('<script src="../js/public/valid.js" type="text/javascript" charset="utf-8"></script>');
 //公共函数&公共路径
-document.write('<script src="./../js/public/public.js?v=20181022" type="text/javascript" charset="utf-8"></script>');
+document.write('<script src="../js/public/public.js?v=20181022" type="text/javascript" charset="utf-8"></script>');
 //切换静态语言包
-document.write('<script src="./../js/jquery.localize.min.js" type="text/javascript" charset="utf-8"></script>'); 
+document.write('<script src="../js/jquery.localize.min.js" type="text/javascript" charset="utf-8"></script>'); 
 //请求语言包json
-// document.write('<script src="./../js/public/getJson.js" type="text/javascript" charset="utf-8"></script>');
+// document.write('<script src="../js/public/getJson.js" type="text/javascript" charset="utf-8"></script>');
